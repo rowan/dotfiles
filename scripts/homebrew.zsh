41ee7911551f605391a134cadc8d369d7bd06734
rowan <rowan@hoku.nz>
2022-04-30 19:24:36 +1200

    Update homebrew.zsh

--- scripts/homebrew.zsh
+++ scripts/homebrew.zsh
@@ -1,4 +1,4 @@
-if (( command -v brew == "" ))
+if (( $(command -v brew) == "" ))
 then
     echo "Installing brew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"

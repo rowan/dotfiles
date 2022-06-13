f8d8171cf2a4fcebd5706f3106f569d1ac4e98de
rowan <rowan@hoku.nz>
2022-06-13 17:31:01 +1200

    Use apply-user-defaults to set macOS defaults

--- scripts/macos-defaults.zsh
+++ scripts/macos-defaults.zsh
@@ -1,50 +1,7 @@
 # Sets macOS defaults.
+apply-user-defaults apps/system/user-defaults.yaml --verbose
 
-# Turn on app auto-update
-defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
-defaults write com.apple.commerce AutoUpdate -bool true
-
-# Use AirDrop over every interface. srsly this should be a default.
-defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
-
-# Always open everything in Finder's column view. This is important.
-defaults write com.apple.Finder FXPreferredViewStyle clmv
-
-# Show the ~/Library folder.
-# chflags nohidden ~/Library
-
-# Save files to disk (rather than iCloud) by default
-defaults write NSGlobalDomain "NSDocumentSaveNewDocumentsToCloud" -bool "false" 
-
-# Disable force click action on trackpad
-defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerTapGesture 2
-
-# Set the Finder prefs for showing a few different volumes on the Desktop.
-defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
-
-# Hide Safari's bookmark bar.
-# defaults write com.apple.Safari ShowFavoritesBar -bool false
-
-# Set up Safari for development.
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-# Avoid creating .DS_Store files on network or USB volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-
-# Terminal theme
-# see: /scripts/terminal.zsh
-# defaults write com.apple.terminal "Default Window Settings" -string "Solarized Dark"
-# defaults write com.apple.Terminal "Startup Window Settings" -string "Solarized Dark"
-
-# Prevent Photos from opening automatically when devices are plugged in
-defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
-
-# Show the dock on the right
-defaults write com.apple.dock "orientation" -string "right" && killall Dock
+killall Dock
+killall Finder
+killall Safari
+open -a Safari

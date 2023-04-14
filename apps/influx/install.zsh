e0e9bf863fc24684468e118efccd6cc416a89295
rowan <rowan@hoku.nz>
2023-04-14 15:03:30 +1200

    Revert "Merge branch 'rowan:main' into main"
    This reverts commit b2ecb2205e6b5f2840873f1acd0e3f080990bd71, reversing
    changes made to 5054a344188b1dfcb5c7f35ccd0661893f386bf4.

--- apps/influx/install.zsh
+++ apps/influx/install.zsh
@@ -1,8 +1,8 @@
-# increase the maximum number of file descriptiors
-sudo cp $DOTFILES/apps/influx/limit.maxfiles.plist /Library/LaunchDaemons/limit.maxfiles.plist
-sudo chown root:wheel /Library/LaunchDaemons/limit.maxfiles.plist
-sudo launchctl unload /Library/LaunchDaemons/limit.maxfiles.plist
-sudo launchctl load -w /Library/LaunchDaemons/limit.maxfiles.plist
+# # increase the maximum number of file descriptiors
+# sudo cp $DOTFILES/apps/influx/limit.maxfiles.plist /Library/LaunchDaemons/limit.maxfiles.plist
+# sudo chown root:wheel /Library/LaunchDaemons/limit.maxfiles.plist
+# sudo launchctl unload /Library/LaunchDaemons/limit.maxfiles.plist
+# sudo launchctl load -w /Library/LaunchDaemons/limit.maxfiles.plist
 
-# restart the service
-brew services restart influxdb
+# # restart the service
+# brew services restart influxdb

42e55ea0c15e07142e3a9cc828fb4664b5625d82
rowan <rowan@hoku.nz>
2026-01-14 16:13:59 +1300

    Skip launchpad for macOS26+

--- scripts/defaults.zsh
+++ scripts/defaults.zsh
@@ -9,8 +9,16 @@ apply-user-defaults apps/system/user-defaults.yaml --verbose
 # Populate dock
 # note: first item below resets existing array settings, others are added to the array
 
-defaults write com.apple.dock persistent-apps -array '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/Launchpad.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Safari.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+macos_version=$(sw_vers -productVersion | cut -d. -f1)
+
+if [[ $macos_version -ge 26 ]]; then
+  # macOS 26+ (Tahoe): Launchpad removed, start with Safari
+  defaults write com.apple.dock persistent-apps -array '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Safari.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+else
+  # macOS 15.x and earlier: Include Launchpad
+  defaults write com.apple.dock persistent-apps -array '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/Launchpad.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+  defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Safari.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
+fi
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Google Chrome.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/Mail.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
 defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/System/Applications/Contacts.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
@@ -29,6 +37,6 @@ defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</
 # Reset
 killall Dock
 killall Finder
-killall Calendar
+killall Calendar 2>/dev/null || true
 
 open -a Safari

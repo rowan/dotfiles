89c1ae320fb60eb5349303cb1f1d2332e05ba971
rowan <rowan@hoku.nz>
2026-01-14 16:57:12 +1300

    Update install.zsh

--- apps/influx/install.zsh
+++ apps/influx/install.zsh
@@ -1,8 +1,9 @@
-# increase the maximum number of file descriptiors
+# increase the maximum number of file descriptors
 sudo cp $DOTFILES/apps/influx/limit.maxfiles.plist /Library/LaunchDaemons/limit.maxfiles.plist
 sudo chown root:wheel /Library/LaunchDaemons/limit.maxfiles.plist
 sudo launchctl unload /Library/LaunchDaemons/limit.maxfiles.plist
 sudo launchctl load -w /Library/LaunchDaemons/limit.maxfiles.plist
 
-# restart the service
-brew services restart influxdb
+# Note: influxdb formula no longer provides a brew service.
+# Start manually with: influxd
+# Or create a custom launchd plist if needed.

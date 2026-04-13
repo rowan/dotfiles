362f5f66c20bc654d08c9d25c5b8aea6f4019c1b
rowan <rowan@hoku.nz>
2026-04-13 15:50:36 +1200

    Update path.zsh

--- apps/yarn/path.zsh
+++ apps/yarn/path.zsh
@@ -1,1 +1,5 @@
-export PATH="$(yarn global bin):$PATH"
+# Only run yarn if we have a valid working directory
+if [[ -d "$PWD" ]] && command -v yarn >/dev/null 2>&1; then
+  YARN_GLOBAL_BIN="$(yarn global bin 2>/dev/null)"
+  [[ -n "$YARN_GLOBAL_BIN" ]] && export PATH="$YARN_GLOBAL_BIN:$PATH"
+fi
